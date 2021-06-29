6520c75d6b5356b49ee9fffc9a437e9c9a337fc5
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-06-29 14:25:20 +0900

    4 commit

---
+++ HelloWorld/StringOperation.java
@@ -0,0 +1,9 @@
+public class StringOperation {
+	public static void main(String[] args) {
+		System.out.println("Hello World".length());
+		//문자열의 길이를 구할 때는 length
+		
+		System.out.println("Hello [[name]]".replace("[[name]]", "eunjeong"));
+		//replace로 문자 변경하기
+	}
+}
