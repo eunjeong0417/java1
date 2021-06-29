71a16418c0773c05223826c1a861f4c633785200
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-06-29 16:11:04 +0900

    project setup

---
+++ HelloWorld/Variable.java
@@ -0,0 +1,13 @@
+public class Variable {
+	public static void main(String[] args) {
+		int a = 1; //integer(정수)
+		System.out.println(a);
+		
+		double b = 1.1;
+		System.out.println(b); //real number -> double(실수)
+		
+		String c = "hello world";
+		System.out.println(c);
+		//변수의 데이터 타입을 명확하게 지정해야 한다
+	}
+}
