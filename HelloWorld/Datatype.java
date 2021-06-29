cbd75f6a55c6c90dcfe013398327275bc0316da9
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-06-29 14:16:23 +0900

    3 commit

--- HelloWorld/Datatype.java
+++ HelloWorld/Datatype.java
@@ -1,6 +1,13 @@
 public class Datatype{
 	public static void main(String[] args){
-		System.out.println(6);
+		System.out.println(6);//Number
+		System.out.println("7");//string
+		
+		System.out.println(6*7);
+		System.out.println("1111".length());
+		System.out.println(Math.PI);
+		System.out.println(Math.floor(Math.PI));
+		System.out.println(Math.ceil(Math.PI));
 	}
 	
-}
+} 
---
+++ HelloWorld/StringApp.java
@@ -0,0 +1,19 @@
+public class StringApp {
+	public static void main(String[] args) {
+		
+		//Character와 String
+		System.out.println("Hello World");//string
+		System.out.println('h');//character
+		
+		
+		//new line
+		System.out.println("hello \nworld");
+		//줄 바꿀때는 \n을 쓴다
+		//n은 new line의 약자
+		
+		//escape
+		System.out.println("hello \"world\" ");
+		//일반적인 문자로 "를 쓰고 싶을때는
+		//"앞에 \를 붙여준다
+	}
+}
